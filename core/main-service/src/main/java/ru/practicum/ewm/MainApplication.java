a34e333e1769ea0b6a7a45f6b7a70c0ae1b015cc
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2025-04-13 12:55:02 +0300

    Исправления по ревью

--- core/main-service/src/main/java/ru/practicum/ewm/MainApplication.java
+++ core/main-service/src/main/java/ru/practicum/ewm/MainApplication.java
@@ -2,14 +2,8 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import ru.practicum.stats.client.StatClient;
-import ru.practicum.stats.dto.HitDto;
-import ru.practicum.stats.dto.StatsRequestParamsDto;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootApplication
 @ComponentScan(value = {"ru.practicum.ewm", "ru.practicum.stats.client"})
