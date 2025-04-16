2c1b4ba8ef4fae8aa2a6a28188c8944c4472fd6e
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2025-04-16 12:19:45 +0300

    Добавлена аннотация в MainApplication

--- core/event-service/src/main/java/ru/practicum/ewm/MainApplication.java
+++ core/event-service/src/main/java/ru/practicum/ewm/MainApplication.java
@@ -2,9 +2,10 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-
+@EnableFeignClients
 @SpringBootApplication
 @ComponentScan(value = {"ru.practicum.ewm", "ru.practicum.stats.client"})
 public class MainApplication {
